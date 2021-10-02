eb9c930a4a0508001566c8086f1db60c3615febd
Manuel A. Buen-Abad <manuel_buen-abad@brown.edu>
2021-10-02 01:12:47 -0400

    green.py: reads Green's Catalog results; mcmc.py: log-likelihoods useful for MCMC analysis; gc_analysis.py: analyzes the Green's Catalog results with an MCMC run, produces output bounds on ga

---
+++ green.py
@@ -0,0 +1,197 @@
+from __future__ import division
+import numpy as np
+from numpy import pi, sqrt, log, log10, power, exp
+from scipy.interpolate import interp1d, interp2d
+import os
+
+# current directory
+current_dir = os.getcwd()
+
+import constants as ct
+import particle as pt
+import astro as ap
+import echo as ec
+import data as dt
+
+# -------------------------------------------------
+
+###############
+# DIRECTORIES #
+###############
+
+green_path = os.path.dirname(os.path.abspath(__file__))+"/output/green_snr/"
+
+
+
+# -------------------------------------------------
+
+##########
+# ARRAYS #
+##########
+
+# loading pre-computed arrays
+pre_Lpk_arr = np.loadtxt(green_path+"Lpk_arr.txt", delimiter=",")
+pre_tpk_arr = np.loadtxt(green_path+"tpk_arr.txt", delimiter=",")
+
+# peak luminosity and time grids
+pre_Lpk_Gr, pre_tpk_Gr = np.meshgrid(pre_Lpk_arr, pre_tpk_arr, indexing='xy')
+
+# normal (0, 1) variables from pre-computed arrays:
+normal_Lpk_arr = (log10(pre_Lpk_arr)-ct._mu_log10_Lpk_)/ct._sig_log10_Lpk_
+normal_tpk_arr = (log10(pre_tpk_arr)-ct._mu_log10_tpk_)/ct._sig_log10_tpk_
+
+# sigmas contours on pre-computed grids
+sigs_Gr = np.sqrt(((log10(pre_Lpk_Gr)-ct._mu_log10_Lpk_)/ct._sig_log10_Lpk_)**2 + ((log10(pre_tpk_Gr)-ct._mu_log10_tpk_)/ct._sig_log10_tpk_)**2)
+
+
+
+# -------------------------------------------------
+
+###############
+# SNR CATALOG #
+###############
+
+# Loading Green's catalog:
+# First let's parse snrs.list.html
+# Names:
+snr_name_arr = dt.load_Green_catalogue_names()
+# Catalog:
+snrs_dct = dt.load_Green_catalogue(snr_name_arr)
+
+# Curating Green's Catalog
+snrs_cut = {}
+snrs_age = {}
+for name, snr in snrs_dct.items():
+    
+    try:
+        snr.distance
+    except:
+        continue
+    
+    try:
+        snr.alpha
+    except:
+        continue
+    
+    if snr.get_flux_density() == -1:
+        continue
+    
+    if not snr.is_flux_certain:
+        continue
+    
+    snrs_cut[name] = snr
+    
+    try:
+        snr.age
+    except:
+        continue
+    
+    if snr.age == None:
+        continue
+    
+    snrs_age[name] = snr
+
+del snr, name
+
+# -------------------------------------------------
+
+#############
+# FUNCTIONS #
+#############
+
+# a small number, but not too small for it to cause overflows when manipulated
+very_small = 1.e-100
+
+
+def load_green_results(name, r=None, tex=0., nuB=1.):
+    """
+    Function that loads the CCV numerical results for the SNRs from Green's Catalog.
+    """
+    
+    if not name in snrs_cut.keys():
+        raise ValueError("name={} not available in results.".format(name))
+    
+    if (r == None) and (not name in snrs_age.keys()):
+        raise ValueError("name={} not available in SNR results of known age.".format(name))
+    
+    if r == None:
+        r_str = "_wage"
+    else:
+        r_str = "_r-{}".format(int(r))
+    
+    tex_str = "_tex-{}".format(int(tex))
+    nuB_str = "_nuB-{}".format(int(nuB))
+    
+    folder = green_path+name+"/"
+    file = "{}_Lpk-tpk{}{}{}.txt".format(name, r_str, tex_str, nuB_str)
+    
+    sn = np.loadtxt(folder+"sn_"+file, delimiter=",")
+    echo = np.loadtxt(folder+"echo_"+file, delimiter=",")
+    
+    if r != None:
+        tgrid = np.loadtxt(folder+"tage_"+file, delimiter=",")
+    else:
+        tgrid = np.loadtxt(folder+"ttrans_"+file, delimiter=",")
+        
+    return sn, echo, tgrid
+
+
+
+def snr_reach(name, r=None, nuB=1., tex=0., sn_ratio_threshold=1., nu_pivot=1., ga_ref=1.e-10, full_output=False):
+    """
+    Returns an interpolated function of the discovery reach of the axion-photon coupling ga [GeV^-1] as a function of the Bietenholz parameters (be that in terms of normalized (0, 1) variables (variables='normal'), or in terms of the raw parameters t_peak and L_peak themselves) for a certain signal-to-noise ratio and SNR name. If full_output == True, it also returns other important quantities.
+    """
+    
+    if (not name in snrs_cut.keys()):
+        raise ValueError("name={} not available in results.".format(name))
+    
+    if (r == None) and (not name in snrs_age.keys()):
+        raise ValueError("name={} not available in SNR results of known age.".format(name))
+    
+    snr = snrs_cut[name]
+    # SNR results
+    sn_Gr, _, tgrid = load_green_results(name, r=r, tex=tex, nuB=nuB)
+    # N.B.: tgrid is t_trans if r==None, and t_age if r!= None
+    
+    # SNR properties:
+    alpha = snr.alpha
+    gamma = ap.gamma_from_alpha(alpha)
+    S0 = snr.get_flux_density() # [Jy] spectral irrad. today
+    L0 = snr.get_luminosity() # [cgs]
+    size = snr.sr
+
+    # going from Bietenholz peak Luminosities to pivot luminosities (at 1 GHz):
+    # conversion factor:
+    from_Bieten_to_pivot = (nu_pivot/nuB)**-alpha
+    # copying arrays and grids:
+    Lpk_arr, tpk_arr = np.copy(pre_Lpk_arr), np.copy(pre_tpk_arr)
+    Lpk_Gr, tpk_Gr = np.copy(pre_Lpk_Gr), np.copy(pre_tpk_Gr)
+    # correcting with conversion factor:
+    Lpk_arr *= from_Bieten_to_pivot
+    Lpk_Gr *= from_Bieten_to_pivot
+    
+    # normal_Lpk value where the cut takes place
+    normal_Lpk_cut = (log10(L0/from_Bieten_to_pivot)-ct._mu_log10_Lpk_)/ct._sig_log10_Lpk_
+    
+    # computing the forbidden parameter space region
+    nonsense_lum = (L0 >= Lpk_Gr).astype(int) # points where L0 >= Lpk
+    if r == None:
+        nonsense_time = np.zeros_like(tpk_Gr) # TODO: change?
+    else:
+        nonsense_time = np.zeros_like(tpk_Gr) # TODO: change?
+#         tt_Gr = r*(tpk_Gr/365.)
+#         nonsense_time = (tgrid < tt_Gr).astype(int) # points where t_age < t_trans
+    
+    nonsense_params = np.logical_or(nonsense_lum, nonsense_time).astype(int)
+    
+    regularized_sn_Gr = np.where(sn_Gr < very_small, very_small, sn_Gr) # converting 0s to a small number
+    
+    ga_Gr = ec.ga_reach(sn_ratio_threshold, regularized_sn_Gr, ga_ref)
+    ga_Gr = np.nan_to_num(ga_Gr)
+    
+    def ga_fn(nL, nt): return 10.**interp2d(normal_Lpk_arr, normal_tpk_arr, log10(ga_Gr))(nL, nt)
+    
+    if full_output:
+        return ga_fn, ga_Gr, normal_Lpk_arr, normal_tpk_arr, nonsense_params
+    else:
+        return ga_fn
---
+++ mcmc.py
@@ -0,0 +1,128 @@
+from __future__ import division
+import numpy as np
+from numpy import pi, sqrt, log, log10, power, exp
+from scipy.interpolate import interp2d
+import os
+import emcee
+
+# current directory
+current_dir = os.getcwd()
+
+
+
+# -------------------------------------------------
+
+#################
+# MCMC ANALYSIS #
+#################
+
+# a large number, but not too large for it to cause overflows when manipulated
+very_large = 1.e100
+
+
+
+def normal_prior(x, limits):
+    """
+    Prior on normalized variable x, with limits.
+    """
+    
+    return -very_large*np.logical_not(np.heaviside(x-limits[0], 1.)*np.heaviside(limits[1]-x, 1.))
+
+
+
+def forbidden_prior(x, y, xarr, yarr, nonsense_grid):
+    """
+    Prior for forbidden/nonsensical parameters, obtained from a grid whose values are 0 (allowed) or 1 (forbidden).
+    """
+    
+    def nonsense_fn(x, y): return interp2d(xarr, yarr, nonsense_grid)(x, y)
+    
+    return -very_large*nonsense_fn(x, y)
+
+
+
+def log_likelihood(nL, nt):
+    """
+    Log-likelihood for a 2D normal distribution.
+    """
+    
+    return -0.5*(nL**2. + nt**2.)
+
+
+
+def log_posterior(nL, nt, nL_limits, nt_limits, blob_fn, forbidden=False, **nonsense_kwargs):
+    """
+    Computes the posterior from the priors and the log-likelihood.
+    """
+    
+    nL_prior = normal_prior(nL, nL_limits)
+    nt_prior = normal_prior(nt, nt_limits)
+    
+    if forbidden:
+        no_prior = forbidden_prior(nL, nt, **nonsense_kwargs)
+    else:
+        no_prior = 0.
+    
+    loglkl = log_likelihood(nL, nt)    
+    
+    posterior = nL_prior + nt_prior + no_prior + loglkl
+    
+#     print nL_prior, nt_prior, no_prior, loglkl, posterior
+    
+    blob = blob_fn(nL, nt)[0]
+    
+    return posterior, blob
+
+
+
+def snr_emcee_routine(ga_fn,
+                      nL_limits=(None, None),
+                      nt_limits=(None, None),
+                      forbidden=False,
+                      nwalkers=50, nburn=200, nsteps=2000,
+                      **nonsense_kwargs):
+    """
+    The emcee routine to analyze the Green's Catalog SNR results.
+    """
+    
+    # limits of the priors
+    nL_center, nL_size = np.mean(nL_limits), nL_limits[1] - nL_limits[0]
+    nt_center, nt_size = np.mean(nt_limits), nt_limits[1] - nt_limits[0]
+    
+    # two dimensions
+    ndim = 2
+    
+    # starting guesses
+    p0 = np.random.rand(nwalkers, ndim)-0.5
+    # for normalized Lpk:
+    p0[:,0] *= nL_size # with correct size...
+    p0[:,0] += nL_center # ... and center
+    # for normalized tpk:
+    p0[:,1] *= nt_size # with correct size...
+    p0[:,1] += nt_center # ... and center
+    
+    # preparing the data type of the blobs
+    dtype = [("ga_reach", float)]
+    
+    # defining the posterior probability
+    def log_post(x):
+        return log_posterior(x[0], x[1],
+                             nL_limits, nt_limits,
+                             ga_fn,
+                             forbidden=forbidden,
+                             **nonsense_kwargs)
+    
+    sampler = emcee.EnsembleSampler(nwalkers, ndim, log_post,
+                                    blobs_dtype=dtype)
+    sampler.run_mcmc(p0, nsteps)
+    
+    sample = sampler.chain # shape = (nwalkers, nsteps, ndim)
+    sample = sampler.chain[:, nburn:, :] # discard burn-in points
+    sample = sample.reshape(-1, ndim)
+    
+    blobs = sampler.get_blobs()
+    ga_reach = blobs["ga_reach"]
+    
+    ga_reach = ga_reach[nburn:,:].ravel()
+    
+    return sample, ga_reach
